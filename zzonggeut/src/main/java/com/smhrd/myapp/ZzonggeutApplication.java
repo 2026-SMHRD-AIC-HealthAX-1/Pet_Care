d84e6c88e3425c3fac3721d929df4c13290fa569
smhrd <oyj7417@gmail.com>
2026-09-21 11:57:21 +0900

    오용준 - AI서버에서 업로드 모드로 데이터를 스프링서버로 보내고 스프링서버네어 수신받은 메시지 확인하는거까지 작업됨

--- zzonggeut/src/main/java/com/smhrd/myapp/ZzonggeutApplication.java
+++ zzonggeut/src/main/java/com/smhrd/myapp/ZzonggeutApplication.java
@@ -2,7 +2,9 @@ package com.smhrd.myapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling; // 추가
 
+@EnableScheduling // 추가
 @SpringBootApplication
 public class ZzonggeutApplication {
 

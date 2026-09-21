0ea8b70c358ae4ecd603e5e56eae35f8ba3228fa
smhrd <oyj7417@gmail.com>
2026-09-21 14:27:03 +0900

    오용준 - AI서버(VS Code)에서 업로드 모드로 Spring서버로 보내서 DB에 저장하기 완료

--- zzonggeut/src/main/java/com/smhrd/myapp/ZzonggeutApplication.java
+++ zzonggeut/src/main/java/com/smhrd/myapp/ZzonggeutApplication.java
@@ -1,11 +1,13 @@
 package com.smhrd.myapp;
 
 import org.springframework.boot.SpringApplication;
+//import org.mybatis.spring.annotation.MapperScan; // 임포트 추가
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling; // 추가
 
 @EnableScheduling // 추가
 @SpringBootApplication
+//@MapperScan("com.smhrd.myapp.mapper") // <- 이 한 줄 추가
 public class ZzonggeutApplication {
 
 	public static void main(String[] args) {
